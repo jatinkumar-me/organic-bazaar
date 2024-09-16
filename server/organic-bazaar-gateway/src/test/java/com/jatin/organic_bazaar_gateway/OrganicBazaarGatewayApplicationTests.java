84ddfcc264e6c1da7a14c7757de02b1340130779
Jatin Kumar <jatin208015@gmail.com>
2024-09-16 13:07:05 +0530

    gateway initial commit

---
+++ server/organic-bazaar-gateway/src/test/java/com/jatin/organic_bazaar_gateway/OrganicBazaarGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jatin.organic_bazaar_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganicBazaarGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
