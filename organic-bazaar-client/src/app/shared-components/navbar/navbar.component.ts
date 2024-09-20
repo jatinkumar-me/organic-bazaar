6239f6a7906375960788ea0e30e3fa6cbb902f29
Jatin Kumar <jatin208015@gmail.com>
2024-09-21 00:23:44 +0530

    navbar cart

--- organic-bazaar-client/src/app/shared-components/navbar/navbar.component.ts
+++ organic-bazaar-client/src/app/shared-components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { CartService } from '../../cart/cart.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,13 +10,26 @@ import { AuthService } from '../../auth/auth.service';
 export class NavbarComponent implements OnInit {
   isLoggedIn: boolean = false;
   username: string | undefined;
+  cartItemCount = 0;
+  showModal = false;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private cartService: CartService) { }
 
   ngOnInit(): void {
-      this.authService.currentUser.subscribe(user => {
+    this.authService.currentUser.subscribe(user => {
       this.isLoggedIn = user != null;
       this.username = user?.name;
-    })
+    });
+    this.cartService.cartSubject.subscribe(cart => {
+      this.cartItemCount = cart.size;
+    });
+  }
+
+  openCart(): void {
+    this.showModal = true;
+  }
+
+  closeCart(): void {
+    this.showModal = false;
   }
 }
