9cd79c497119fc55b0cf454894b6b7c9e8484cb0
Jatin Kumar <jatin208015@gmail.com>
2024-09-22 22:44:57 +0530

    auth test

--- organic-bazaar-client/src/app/auth/auth.guard.spec.ts
+++ organic-bazaar-client/src/app/auth/auth.guard.spec.ts
@@ -1,17 +1,53 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
-import { authGuard } from './auth.guard';
-
-describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+describe('AuthGuard', () => {
+  let authGuard: AuthGuard;
+  let authService: jasmine.SpyObj<AuthService>;
+  let router: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['isLoggedIn']);
+    const routerSpy = jasmine.createSpyObj('Router', ['createUrlTree']);
+
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [
+        AuthGuard,
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: Router, useValue: routerSpy }
+      ]
+    });
+
+    authGuard = TestBed.inject(AuthGuard);
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
   });
 
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+  it('should allow the authenticated user to activate the route', () => {
+    authService.isLoggedIn.and.returnValue(true);
+
+    const result = authGuard.canActivate({} as any, { url: '/home' } as any);
+
+    expect(result).toBeTrue();
+    expect(authService.isLoggedIn).toHaveBeenCalled();
+  });
+
+  it('should prevent access and redirect to the login page for unauthenticated users', () => {
+    authService.isLoggedIn.and.returnValue(false);
+
+    const mockUrlTree = {} as any;
+    router.createUrlTree.and.returnValue(mockUrlTree);
+
+    const result = authGuard.canActivate({} as any, { url: '/home' } as any);
+
+    expect(result).toBe(mockUrlTree);
+    expect(authService.isLoggedIn).toHaveBeenCalled();
+    expect(router.createUrlTree).toHaveBeenCalledWith(['/login'], {
+      queryParams: { returnUrl: '/home' }
+    });
   });
 });
